993a91ac0f9e2197a0ea9f1ca57d4013f022c35e
smallzeno <sohjungchampion@gmail.com>
2021-08-12 16:37:35 +0100

    🥺 figure it out why do i need this.

--- packages/web/src/react-app-env.d.ts
+++ packages/web/src/react-app-env.d.ts
@@ -1,1 +1,2 @@
 /// <reference types="react-scripts" />
+//not needed fixes dependency issue
--- packages/web/src/setupTests.ts
+++ packages/web/src/setupTests.ts
@@ -3,3 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// i dunno why this is needed, but it fixes the "unable to find module './src/setupTests.ts'" error
+// even tho i removed the deps from package.json and index.tsx
